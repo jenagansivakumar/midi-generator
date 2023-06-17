67d0a65793d101aff7f7a79efce0fb895490c7b3
Jena <jenagansivakumar@gmail.com>
2023-06-17 13:20:58 +0200

    added tone.js

---
+++ my-react-app/src/Components/MidiGenerator.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+import * as tone from "tone";
+// import { OpenAIApi } from "@openai/api";
+
+export default function MidiGenerator() {
+  return <div>MidiGenerator</div>;
+}
